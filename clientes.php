19e67549eea66698279f06e3a9e37773e695ecf6
jusiho <pierreblas123@gmail.com>
2022-03-19 16:18:16 -0500

    cambio pro

--- clientes.php
+++ clientes.php
@@ -3,39 +3,36 @@
     echo "<h1>".get_admin_page_title()."</h1>";
     global $wpdb;
 
-    $query = "SELECT sum(num_items_sold) AS hola , customer_id, status FROM {$wpdb->prefix}wc_order_stats GROUP BY customer_id, status HAVING status != 'wc-on-hold'";
+    $query = "SELECT sum(wo.num_items_sold) as sum_items, wo.customer_id, wo.status,wc.username as username, wc.user_id as id_user FROM {$wpdb->prefix}wc_order_stats as wo INNER JOIN {$wpdb->prefix}wc_customer_lookup AS wc ON wo.customer_id = wc.customer_id GROUP BY wo.customer_id, status HAVING wo.status in ('wc-processing','wc-completed')";
+       
     $lista = $wpdb->get_results($query,ARRAY_A);
-    echo var_dump($lista);
-    foreach ($lista as $key => $value) {
-        $custome = $value['hola'];
-        echo 'Vakis : '.$custome. '<br>';
-    }
+
 	$customer_ids = $wpdb->get_col("SELECT DISTINCT meta_value  FROM $wpdb->postmeta
     WHERE meta_key = '_customer_user' AND meta_value > 0");
 
     foreach ($customer_ids as $customer_id) {
     $customer = new WP_User($customer_id);
+    //var_dump($customer);
 }
 ?>
 <a class="page-title-action">Añadir nueva</a>    
 <br><br><br>
 <table class="wp-list-table widefat fixed striped page">
     <thead>
+        <th>ID</th>
         <th>Nombre de Empleado</th>
-        <th>Correo </th>
         <th>Precio total</th>
         <th>Cantidad de tarjetas</th>
         <th>Accion</th>
     </thead>
      
     <tbody>
-        <?php foreach ($customer_ids as $customer_id) :
-            $customer = new WP_User($customer_id); ?>
+        <?php foreach ($lista as $key => $value) :?>
             <tr>
-                <td><?php echo $customer->display_name; ?></td>
-                <td><?php echo $customer->user_email; ?></td>
-                <td><?php echo wc_get_customer_total_spent($customer_id); ?></td>
-                <td></td>
+                <td><?php echo $value['id_user']; ?></td>
+                <td><?php echo $value['username']; ?></td>
+                <td><?php echo wc_get_customer_total_spent($value['id_user']); ?></td>
+                <td><?php echo $value['sum_items']; ?></td>
                 <td><a class="page-title-action">Ver</a></td>
             </tr>
         <?php endforeach; ?>
--- tarjetas.php
+++ tarjetas.php
@@ -12,6 +12,8 @@ function activar(){
 	if (get_option('tarjetas_cuenta')=== false){
 		add_option('tarjetas_cuenta',"HOLA");
 	}
+
+	
 }
 function menu_ajuste_tarjetas(){
 	add_menu_page('Cuentas de tarjetas',
@@ -33,6 +35,7 @@ function menu_ajuste_tarjetas(){
 	);
  */
 }
+
 add_action('admin_menu','menu_ajuste_tarjetas');
 
 function genera_hmtl_pagina(){
@@ -187,10 +190,52 @@ function my_account_new_endpoints()
  * Recuperando el contenido de la nueva variable
  */
 // Premios
+
+function create_nuevo_vc(){
+    global $wp;
+    $content = "BEGIN:VCARD\r\n";
+    $content .= "VERSION:3.0\r\n";
+    $content .= "CLASS:PUBLIC\r\n";
+    $content .= "FN:Joe Wegner\r\n";
+    $content .= "N:Wegner;Joe ;;;\r\n";
+    $content .= "TITLE:Technology And Systems Administrator\r\n";
+    $content .= "ORG:Wegner Design\r\n";
+    $content .= "ADR;TYPE=work:;;21 W. 20th St.;Broadview ;IL;60559;\r\n";
+    $content .= "EMAIL;TYPE=internet,pref:joe@wegnerdesign.com\r\n";
+    $content .= "TEL;TYPE=work,voice:7089181512\r\n";
+    $content .= "TEL;TYPE=HOME,voice:8352355189\r\n";
+    $content .= "URL:http://www.wegnerdesign.com\r\n";
+    $content .= "END:VCARD\r\n";
+	
+	$url_actual = home_url();
+    $file_name = dirname(__FILE__);
+    //$file = fopen($file_name,"w"); // W de Write
+	// $file = fopen($file_name, 'w');
+    // fwrite($file, $content);
+    // fclose($file);
+
+	// path to admin/
+	$this_dir = dirname(__FILE__);
+	$parent_dir = realpath($this_dir . '/..');
+	$target_path = $parent_dir . '/tarjetas/' . 'pierre.vcf';
+	$ourFileHandle = fopen($target_path, 'w') or die("can't open file");
+	
+	fwrite($ourFileHandle, $content);
+	fclose($ourFileHandle);
+	
+	echo '<h1>SE CREO VCF'.$file_name.'</h1>';
+}
+
 add_action('woocommerce_account_regalos_endpoint', 'regalos_endpoint_content');
 function regalos_endpoint_content()
 {
 	//echo 'tarjeta';
+	create_nuevo_vc();
+
+	if (!is_dir('users/')) {
+		mkdir('users/');
+		echo '<h1>HOLA QUE HACE</h1>';
+	}
 	echo do_shortcode('[productos_cliente]');
 	//get_template_part('mis-tarjetas');
 }
@@ -210,7 +255,6 @@ function my_account_menu_order()
 	return $menuOrder;
 }
 add_filter('woocommerce_account_menu_items', 'my_account_menu_order');
-
 
 add_action('wp_enqueue_scripts','carga_stilos');
 
