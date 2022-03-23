c3148fac374b48049ec701c2704859f872f65a07
michaelrm0 <michaelrm0@outlook.com>
2022-03-22 22:39:50 -0500

    Elimina la página

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ register_activation_hook(__FILE__, 'add_page_view_contact');
 register_deactivation_hook(__FILE__, 'drop_table_vcards');
 register_deactivation_hook(__FILE__, 'drop_directory_vcards');
 register_deactivation_hook(__FILE__, 'drop_directory_photos');
-
+register_deactivation_hook( __FILE__, 'delete_page_view_contact' );
 
 function insert_contact($atts)
 {
