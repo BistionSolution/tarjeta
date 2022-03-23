8b497d6dc08f64c47ee43cbea59de12990a1ae60
michaelrm0 <michaelrm0@outlook.com>
2022-03-22 21:51:10 -0500

    arreglo en el js del límite

--- assets/imagen.js
+++ assets/imagen.js
@@ -1,22 +1,23 @@
 var imagen = document.getElementById("file_img");
-				var btnActualizar = document.getElementById('btn-actualizar');
-				imagen.onchange = function(e) {
-					let reader = new FileReader();
-					var peso = e.target.files[0].size; //peso en bytes
-					reader.readAsDataURL(e.target.files[0]);
-					reader.onload = function(){
-						let preview = document.getElementById('preview'),
-								image = document.createElement('img');
-						image.src = reader.result;
-						preview.innerHTML = '';
-						preview.append(image);
-					};
-					btnActualizar.disabled = false;
-					let texto = document.getElementById('text-img');
-					texto.innerHTML = '2097152';
-					if (peso>250000){ //Limitar el peso		
-						texto.append('El peso de la imagen excede el permitido.');
-						texto.style.color='#ff0000'
-						btnActualizar.disabled = true;			
-					}
-				}
+var btnActualizar = document.getElementById('btn-actualizar');
+imagen.onchange = function(e) 
+{
+	let reader = new FileReader();
+	var peso = e.target.files[0].size; //peso en bytes
+	reader.readAsDataURL(e.target.files[0]);
+	reader.onload = function(){
+		let preview = document.getElementById('preview'),
+				image = document.createElement('img');
+		image.src = reader.result;
+		preview.innerHTML = '';
+		preview.append(image);
+	};
+	btnActualizar.disabled = false;
+	let texto = document.getElementById('text-img');
+	texto.innerHTML = '';
+	if (peso>2097152){ //Limitar el peso		
+		texto.append('El peso de la imagen excede el permitido(2MB).');
+		texto.style.color='#ff0000'
+		btnActualizar.disabled = true;			
+	}
+}
