a1ac71980f1c04ffe51472074112bcaecaf18651
michaelrm0 <michaelrm0@outlook.com>
2022-03-30 19:27:37 -0500

    comentando la eliminacion de la tabla vcard y carpetas generadas

--- delete-vcard.php
+++ delete-vcard.php
@@ -1,46 +1,46 @@
 <?php
 
-function drop_table_vcards()
-{
-	global $wpdb;
-	$table_name = $wpdb->prefix . "vcards";
-	$sql = "DROP TABLE IF EXISTS $table_name";
+// function drop_table_vcards()
+// {
+// 	global $wpdb;
+// 	$table_name = $wpdb->prefix . "vcards";
+// 	$sql = "DROP TABLE IF EXISTS $table_name";
 	
-	$wpdb->query($sql);
-}
+// 	$wpdb->query($sql);
+// }
 
-function drop_directory_vcards()
-{
-	$path_directory = realpath(dirname(__FILE__) . '/../../..') . '/wp-vcards';
-	if (file_exists($path_directory)){
-		foreach(glob($path_directory . "/*") as $archivos_carpeta){             
-			if (is_dir($archivos_carpeta)){
-				drop_directory_vcards($archivos_carpeta);
-			} else {
-				unlink($archivos_carpeta);
-			}
-		}	
-		rmdir($path_directory);
-	}
+// function drop_directory_vcards()
+// {
+// 	$path_directory = realpath(dirname(__FILE__) . '/../../..') . '/wp-vcards';
+// 	if (file_exists($path_directory)){
+// 		foreach(glob($path_directory . "/*") as $archivos_carpeta){             
+// 			if (is_dir($archivos_carpeta)){
+// 				drop_directory_vcards($archivos_carpeta);
+// 			} else {
+// 				unlink($archivos_carpeta);
+// 			}
+// 		}	
+// 		rmdir($path_directory);
+// 	}
 	
-}
+// }
 
-function drop_directory_photos($path_directory)
-{
-	if (file_exists($path_directory))
-	{
-		$path_directory = realpath(dirname(__FILE__) . '/../../..') . '/wp-photos';
-		foreach(glob($path_directory . "/*") as $archivos_carpeta){             
-			if (is_dir($archivos_carpeta)){
-				drop_directory_photos($archivos_carpeta);
-			} else {
-				unlink($archivos_carpeta);
-			}
-		}	
-		rmdir($path_directory);    
-	}
+// function drop_directory_photos($path_directory)
+// {
+// 	if (file_exists($path_directory))
+// 	{
+// 		$path_directory = realpath(dirname(__FILE__) . '/../../..') . '/wp-photos';
+// 		foreach(glob($path_directory . "/*") as $archivos_carpeta){             
+// 			if (is_dir($archivos_carpeta)){
+// 				drop_directory_photos($archivos_carpeta);
+// 			} else {
+// 				unlink($archivos_carpeta);
+// 			}
+// 		}	
+// 		rmdir($path_directory);    
+// 	}
 	 
-}
+// }
 
 // function delete_page_view_contact()
 // {
