4e61dd80ac08f2081ef16924e7873cfea8d0c2f9
jusiho <pierreblas123@gmail.com>
2022-03-31 07:40:10 -0500

    vamos git

--- page-vcard.php
+++ page-vcard.php
@@ -160,7 +160,7 @@ function page_vcard()
             global $wp_query;
             $wp_query->set_404();
             status_header( 404 );
-            get_template_part( 404 ); exit();
+            // get_template_part( 404 ); exit();
         endif;
     else:
         // $atts = [
@@ -172,7 +172,7 @@ function page_vcard()
         global $wp_query;
         $wp_query->set_404();
         status_header( 404 );
-        get_template_part( 404 ); exit();
+        // get_template_part( 404 ); exit();
     endif;
 }
     
