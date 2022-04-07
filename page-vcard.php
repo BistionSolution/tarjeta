42d423f1e17e0349d4080944cd10545898b6cb8a
jusiho <pierreblas123@gmail.com>
2022-04-07 06:38:31 -0500

    go

--- page-vcard.php
+++ page-vcard.php
@@ -231,7 +231,7 @@ function page_vcard()
             global $wp_query;
             $wp_query->set_404();
             status_header( 404 );
-            get_template_part( 404 ); exit();
+            // get_template_part( 404 ); exit();
         endif;
     else:
         // $atts = [
@@ -243,7 +243,7 @@ function page_vcard()
         global $wp_query;
         $wp_query->set_404();
         status_header( 404 );
-        get_template_part( 404 ); exit();
+        // get_template_part( 404 ); exit();
     endif;
 }
     
