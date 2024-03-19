b02ef83705ae7be24001674a9ea29c2e09ba8eac
jusiho <pierreblas123@gmail.com>
2024-03-18 21:56:37 -0500

    goes

--- controllers/goes.php
+++ controllers/goes.php
@@ -297,35 +297,36 @@ function div_go($mi_variable, $column_name)
                                     </div>
                                 <?php endif; ?>
 
-                                <div>
-                                    <h2>Video Youtube</h2>
-                                    <?php if (!empty($video_youtube)) :
-                                        $cantidad_url_video     = strlen($video_youtube);
-                                        if ($cantidad_url_video == '28') {
-                                            $cortar_url             = str_replace('https://youtu.be/', '', $video_youtube);
-                                            $url_final_video         = 'https://www.youtube.com/embed/' . $cortar_url;
-                                        } elseif ($cantidad_url_video == '41') {
-                                            $cortar_url = str_replace('https://m.youtube.com/watch?v=', '', $video_youtube);
-                                            $url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
-                                        } elseif ($cantidad_url_video == '43') {
-                                            $cortar_url = str_replace('https://www.youtube.com/watch?v=', '', $video_youtube);
-                                            $url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
-                                        } elseif ($cantidad_url_video == '58') {
-                                            $cortar_url = str_replace('https://m.youtube.com/watch?v=', '', $video_youtube);
-                                            $url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
-                                        } elseif ($cantidad_url_video == '60') {
-                                            $cortar_url = str_replace('https://www.youtube.com/watch?v=', '', $video_youtube);
-                                            $url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
-                                        } else {
-                                            echo "URL INVALIDA";
-                                        } ?>
 
+                                <?php if (!empty($video_youtube)) :
+                                    $cantidad_url_video     = strlen($video_youtube);
+                                    if ($cantidad_url_video == '28') {
+                                        $cortar_url             = str_replace('https://youtu.be/', '', $video_youtube);
+                                        $url_final_video         = 'https://www.youtube.com/embed/' . $cortar_url;
+                                    } elseif ($cantidad_url_video == '41') {
+                                        $cortar_url = str_replace('https://m.youtube.com/watch?v=', '', $video_youtube);
+                                        $url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
+                                    } elseif ($cantidad_url_video == '43') {
+                                        $cortar_url = str_replace('https://www.youtube.com/watch?v=', '', $video_youtube);
+                                        $url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
+                                    } elseif ($cantidad_url_video == '58') {
+                                        $cortar_url = str_replace('https://m.youtube.com/watch?v=', '', $video_youtube);
+                                        $url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
+                                    } elseif ($cantidad_url_video == '60') {
+                                        $cortar_url = str_replace('https://www.youtube.com/watch?v=', '', $video_youtube);
+                                        $url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
+                                    } else {
+                                        echo "URL INVALIDA";
+                                    } ?>
+                                    <div class="video-yutu">
+                                        <h2>Video Youtube</h2>
                                         <div class="video-youtube">
                                             <iframe width="100%" height="315" src="<?= $url_final_video ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                         </div>
-                                    <?php endif; ?>
+                                    </div>
 
-                                </div>
+                                <?php endif; ?>
+
 
                                 <?php if (!empty($spotify) && (!empty($apple_music))) : ?>
                                     <div class="musica">
